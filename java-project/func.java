6018e507d69f7da11303a11c0560a9055d176626
delicious <delicious@deliciousdeMacBook-Pro.local>
2017-12-11 00:42:58 +0800

    code

---
+++ java-project/func.java
@@ -0,0 +1,22 @@
+public class func
+{
+    public static void main(String[] args) {
+
+
+    }
+}
+
+class student
+{
+    int age;
+    String name;
+    int fee;
+
+    static int totalFee;
+}
+
+class school
+{
+    
+}
+
